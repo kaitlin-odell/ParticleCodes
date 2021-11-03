286a75e98d099449d8a8830d8cb927bdd28fcf2f
kaitlin-odell <kodell@gmail.com>
2021-11-03 15:47:11 -0600

    adjustments to L2 and MMD to plot for iterations and particles

--- L2norm.m
+++ L2norm.m
@@ -7,18 +7,35 @@ clear;
 %env_name = 'sine';
 env_name = 'star';
 
-n_particles = 50;  %number of particles
+n_particles = 200;  %number of particles
 dim = 2;            %dimension of the problem
-outer_iter = 50;
+outer_iter = 1:200;
+L2_iter = zeros(1,200);
 
 %%Compute points in the un-normalized target distribution for MMD calculation
-M = 50;
-y = sample_star(100,5,M);
+y = sample_star(100,5,n_particles);
 
-x_evi = trainer(env_name, n_particles, outer_iter);
-L2 = norm(y-x_evi);
+for i = 1:200
+    x_evi = trainer(env_name, n_particles, outer_iter(i));
+    L2_iter(i) = norm(y-x_evi,2)/n_particles;
+end
 
-%%%Plot the target distribution and evi particles%%%
-% figure(1);
-% hold on
-% plot(x,L2)
+%%%Plot the L2 vs number of iterations%%%
+figure(1);
+hold on
+plot(outer_iter,L2_iter)
+
+n_particles = [4 16 64 256 1024 4056]; 
+outer_iter = 50;
+L2_particles = zeros(1,length(n_particles));
+
+for i = 1:length(n_particles)
+    x_evi = trainer(env_name, n_particles(i), outer_iter);
+    y = sample_star(100,5,n_particles(i));
+    L2_particles(i) = norm(y-x_evi, 2)/n_particles(i);
+end
+
+%%%Plot the MMD^2 vs the number of particles%%%
+figure(2);
+hold on
+plot(n_particles,L2_particles)
--- mmd_plot.m
+++ mmd_plot.m
@@ -9,19 +9,33 @@ env_name = 'star';
 
 n_particles = 200;  %number of particles
 dim = 2;            %dimension of the problem
-outer_iter = 1:50;
-MMD = zeros(1,200);
+outer_iter = 1:200;
+MMD1 = zeros(1,200);
 
 %%Compute points in the un-normalized target distribution for MMD calculation
 M = 5000;
 y = sample_star(100,5,M);
 
-for i = 1:50
+for i = 1:200
     x_evi = trainer(env_name, n_particles, outer_iter(i));
-    MMD(i) = mmd(x_evi, y, n_particles, M);
+    MMD1(i) = mmd(x_evi, y, n_particles, M);
 end
 
-%%%Plot the target distribution and evi particles%%%
+%%%Plot the MMD^2 vs number of iterations%%%
 figure(1);
 hold on
-plot(x,MMD)
+plot(outer_iter,MMD1)
+
+n_particles = [4 16 64 256 1024 4056]; 
+outer_iter = 50;
+MMD2 = zeros(1,length(n_particles));
+
+for i = 1:length(n_particles)
+    x_evi = trainer(env_name, n_particles(i), outer_iter);
+    MMD2(i) = mmd(x_evi, y, n_particles(i), M);
+end
+
+%%%Plot the MMD^2 vs the number of particles%%%
+figure(2);
+hold on
+plot(n_particles,MMD2)
